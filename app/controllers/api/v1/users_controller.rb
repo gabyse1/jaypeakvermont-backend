26d552246c9954b93bf524df8880ac5aba6062ad
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-28 04:57:28 -0500

    Generate controllers by using scaffold.

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::UsersController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'users/show'
+      get 'users/create'
+      get 'bookings/index'
+      get 'bookings/create'
+      get 'tours/index'
+      get 'tours/show'
+      get 'tours/create'
+      get 'tours/destroy'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
