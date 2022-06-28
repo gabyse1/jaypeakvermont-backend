f58cb5037048c7a1434589553ab1620bf5985935
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-28 05:04:29 -0500

    Fix resources method.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources: :users, only: %i[show create]
-      resources: :tours, only: %i[index show create destroy]
-      resources: :booking, only: %i[index create]
+      resources :users, only: %i[show create]
+      resources :tours, only: %i[index show create destroy]
+      resources :booking, only: %i[index create]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
