5aeeaab30d9f56302a24e7feb861d789d7a8af1b
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-28 11:38:24 -0500

    Fix linter errors.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true,
-                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: '%<value>s must be a valid email address' }
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+                              message: '%<value>s must be a valid email address' }
   validates :username, presence: true, length: { in: 6..20, message: '%<value>s must be minimum 6 characters' }
 end
